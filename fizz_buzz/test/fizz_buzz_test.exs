874dfa7a5a8f4bf21df347e0c45ccf6e84404c23
Trajano Salgado Reuter Mota <trajanoreuter@gmail.com>
2021-02-10 21:54:20 -0300

    fizz buzz problem

---
+++ fizz_buzz/test/fizz_buzz_test.exs
@@ -0,0 +1,37 @@
+defmodule FizzBuzzTest do
+  use ExUnit.Case
+
+  describe "build/1" do
+    test "when a valid file is provided, return the converted list" do
+      expected_response = {
+        :ok,
+        [
+          1,
+          2,
+          :fizz,
+          4,
+          :fizzbuzz,
+          :fizzbuzz,
+          :fizzbuzz,
+          :fizzbuzz,
+          :fizz,
+          :fizzbuzz,
+          :fizz,
+          :fizzbuzz,
+          :fizzbuzz
+        ]
+      }
+
+      assert FizzBuzz.build("numbers.txt") == expected_response
+    end
+
+    test "when a invalid file is provided, return an error" do
+      expected_response = {
+        :error,
+        "Error reading the file: enoent"
+      }
+
+      assert FizzBuzz.build("invalid.txt") == expected_response
+    end
+  end
+end
